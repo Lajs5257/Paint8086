eed24ade0c7fbc7ec4d72949a93ecf69c5726567
Lajs5257 <lajs52@hotmail.com>
2021-05-17 11:51:54 -0500

    new interface design and relocated colors

--- 8086/Paint.asm
+++ 8086/Paint.asm
@@ -40,11 +40,16 @@ ren1 dw ?
 col2 dw ?
 ren2 dw ?
 color db 0
+herra db 0 
+tam db 3
+
+;0 Sin seleccionar
+;
 ;ren dw ?
 bot dw ?
 c db ?
 r db ?
-;**************************************************************************************************************************
+;**************
 eti0:
 mov ah,3dh ;Funcion 3DH, abre un archivo existente
 mov al,0 ;AL=Modos de Acceso, 0=Solo Lectura, 1=Escritura, 2=Lectura/Escritura
@@ -54,7 +59,7 @@ mov al,0 ;AL=Modos de Acceso, 0=Solo Lectura, 1=Escritura, 2=Lectura/Escritura
 ;(Handle), caso contrario CF=1, y en AX esta el codigo de error
  jc err ;Si hay error, salta a la etiqueta ERR
  mov handle,ax ;Caso contrario HANDLE=Manejador de Archivo
-;************************************************************************************************************************* 
+;*************** 
  mov cx,118d ;Se prepara ciclo de 118 vueltas (Para leer archivo en formato BMP)
 eti1:
  push cx
@@ -65,11 +70,11 @@ eti1:
  int 21h ;INT 21H funcion 3FH, leer del archivo
  pop cx
  loop eti1
-;*************************************************************************************************************************
+;***************
  mov ah,00h ;Funcion 00H para la INT 10H (Resolucion de Pantalla)
  mov al,18d ;AL=Modo de despliegue o resolución, 18 = 640x480 a 16 colores
  int 10h ;INT 10H funcion 00H, inicializar resolucion
- ;***********************************************************************************************************************
+ ;***************
 eti2: 
  mov ah,3fh ;3FH=Leer del archivo
  mov bx,handle
@@ -110,7 +115,7 @@ eti2:
  dec ren
  cmp ren,-1 ;Se compara con -1 para llegar hasta el ultimo renglon, que es el CERO
  jne eti2 ;JNE=Jump if Not Equal (salta si no es igual)
- ;*********************************************************************************************************************** 
+ ;*************** 
  ; Aqui iniciara el proceso para poder validar areas y darle vida a todo esto
  ;mov ah,07h
  jmp inicio
@@ -122,6 +127,7 @@ mov al,18d
 mov c,65d
 mov r,27d
 call pos
+call ColFal
 ;mov ah,09h
 ;lea dx,cadS
 ;int 21h
@@ -155,212 +161,247 @@ jb etiAT
 cmp ren,26d
 ja etiAT
 jmp etiext
-;***********************************************************************************************************************
-;Valicion de area de dibujo
+;***************
+;Validacion de area de dibujo
 etiAT:
 cmp col,150d
-jb etiAC ;JB=Jump if Below (Brinca si esta abajo)
+jb etiAH ;JB=Jump if Below (Brinca si esta abajo)
 cmp col,618d
-ja etiAC ;JA=Jmp if Above (Brinca si esta arriba)
+ja etiAH ;JA=Jmp if Above (Brinca si esta arriba)
 cmp ren,53d
-jb etiAC ;JB=Jump if Below (Brinca si esta abajo)
+jb etiAH ;JB=Jump if Below (Brinca si esta abajo)
 cmp ren,394d
-ja etiAC ;JA=Jmp if Above (Brinca si esta arriba)
-jmp eti4
-;***********************************************************************************************************************
-etiAC:; Area de validacion de todos los colores
-;Validacion para color negro
+ja etiAH ;JA=Jmp if Above (Brinca si esta arriba)
+jmp eti4 
+etiAH:
+; tu validacion de area
+call Barril
+
+etiAC:
+;***************
+; Area de validacion de todos los colores
+;*******Validacion para color negro********+++
+ 
 cmp col,15d
 jb etiC1 ;JB=Jump if Below (Brinca si esta abajo)
 cmp col,35d
 ja etiC1 ;JA=Jmp if Above (Brinca si esta arriba)
 cmp ren,242d
 jb etiC1
-cmp ren,263d
+cmp ren,262d
 ja etiC1
-mov color,0d; Se asigna color Negro
-call ColSel
+mov color,0d ; Se asigna color negro
+call ColSel 
 jmp eti3
-etiC1:
-cmp col,42d
+etiC1: ;*******************   
+
+
+cmp col,43d
 jb etiC2 ;JB=Jump if Below (Brinca si esta abajo)
-cmp col,62d
+cmp col,63d
 ja etiC2 ;JA=Jmp if Above (Brinca si esta arriba)
 cmp ren,242d
 jb etiC2
-cmp ren,263d
-ja etiC2
-mov color,1d; Se asigna color Negro
+cmp ren,262d
+ja etiC2  
+mov color,7d ; Se asigna color Gris
 call ColSel
 jmp eti3
-etiC2:
-cmp col,69d
+etiC2: 
+
+
+cmp col,71d
 jb etiC3 ;JB=Jump if Below (Brinca si esta abajo)
-cmp col,89d
+cmp col,91d
 ja etiC3 ;JA=Jmp if Above (Brinca si esta arriba)
-cmp ren,242d
+cmp ren,244d
 jb etiC3
-cmp ren,263d
+cmp ren,264d
 ja etiC3
-mov color,2d; Se asigna color Negro
+mov color,12d ; Se asigna color Rojo bajo
 call ColSel
 jmp eti3
-etiC3:
+etiC3: 
+
+
 cmp col,96d
 jb etiC4 ;JB=Jump if Below (Brinca si esta abajo)
 cmp col,116d
 ja etiC4 ;JA=Jmp if Above (Brinca si esta arriba)
-cmp ren,242d
+cmp ren,244d
 jb etiC4
-cmp ren,263d
+cmp ren,264d
 ja etiC4
-mov color,3d; Se asigna color Negro
+mov color,4d ; Se asigna color Rojo fuerte
 call ColSel
 jmp eti3
-etiC4:
-cmp col,14d
+etiC4: 
+
+
+cmp col,15d
 jb etiC5 ;JB=Jump if Below (Brinca si esta abajo)
-cmp col,34d
+cmp col,35d
 ja etiC5 ;JA=Jmp if Above (Brinca si esta arriba)
-cmp ren,272d
+cmp ren,273d
 jb etiC5
 cmp ren,293d
 ja etiC5
-mov color,4d; Se asigna color Negro
+mov color,6d ; Se asigna color amarillo fuerte
 call ColSel
 jmp eti3
-etiC5:
-cmp col,42d
+etiC5: 
+
+
+cmp col,43d
 jb etiC6 ;JB=Jump if Below (Brinca si esta abajo)
-cmp col,62d
+cmp col,63d
 ja etiC6 ;JA=Jmp if Above (Brinca si esta arriba)
-cmp ren,271d
+cmp ren,273d
 jb etiC6
-cmp ren,291d
+cmp ren,293d
 ja etiC6
-mov color,5d; Se asigna color Negro
+mov color,10d ; Se asigna color verde
 call ColSel
 jmp eti3
 etiC6:
-cmp col,69d
+
+
+cmp col,71d
 jb etiC7 ;JB=Jump if Below (Brinca si esta abajo)
-cmp col,89d
+cmp col,91d
 ja etiC7 ;JA=Jmp if Above (Brinca si esta arriba)
-cmp ren,271d
+cmp ren,273d
 jb etiC7
-cmp ren,291d
+cmp ren,293d
 ja etiC7
-mov color,6d; Se asigna color Negro
+mov color,15d ; Se asigna color blanco
 call ColSel
 jmp eti3
-etiC7:
-cmp col,96d
+etiC7: 
+
+
+cmp col,99d
 jb etiC8 ;JB=Jump if Below (Brinca si esta abajo)
-cmp col,116d
+cmp col,119d
 ja etiC8 ;JA=Jmp if Above (Brinca si esta arriba)
-cmp ren,271d
+cmp ren,273d
 jb etiC8
-cmp ren,291d
+cmp ren,293d
 ja etiC8
-mov color,7d; Se asigna color Negro
+mov color,14d ; Se asigna color amarillo bajo
 call ColSel
 jmp eti3
-etiC8:
-cmp col,14d
+etiC8: 
+
+cmp col,15d
 jb etiC9 ;JB=Jump if Below (Brinca si esta abajo)
-cmp col,34d
+cmp col,35d
 ja etiC9 ;JA=Jmp if Above (Brinca si esta arriba)
-cmp ren,302d
+cmp ren,303d
 jb etiC9
-cmp ren,322d
+cmp ren,323d
 ja etiC9
-mov color,8d; Se asigna color Negro
+mov color,1d ; Se asigna color azul fuerte
 call ColSel
 jmp eti3
-etiC9:
-cmp col,42d
+etiC9:   
+
+
+cmp col,43d
 jb etiC10 ;JB=Jump if Below (Brinca si esta abajo)
-cmp col,62d
+cmp col,63d
 ja etiC10 ;JA=Jmp if Above (Brinca si esta arriba)
-cmp ren,302d
+cmp ren,303d
 jb etiC10
-cmp ren,322d
+cmp ren,323d
 ja etiC10
-mov color,9d; Se asigna color Negro
+mov color,9d ; Se asigna color azul medio 
 call ColSel
 jmp eti3
 etiC10:
-cmp col,70d
+
+
+cmp col,71d
 jb etiC11 ;JB=Jump if Below (Brinca si esta abajo)
-cmp col,90d
+cmp col,91d
 ja etiC11 ;JA=Jmp if Above (Brinca si esta arriba)
-cmp ren,301d
+cmp ren,303d
 jb etiC11
-cmp ren,321d
+cmp ren,323d
 ja etiC11
-mov color,10d; Se asigna color Negro
+mov color,5d ; Se asigna color morado fuerte 
 call ColSel
 jmp eti3
-etiC11:
-cmp col,96d
+etiC11:    
+
+
+cmp col,98d
 jb etiC12 ;JB=Jump if Below (Brinca si esta abajo)
-cmp col,116d
+cmp col,118d
 ja etiC12 ;JA=Jmp if Above (Brinca si esta arriba)
-cmp ren,301d
+cmp ren,303d
 jb etiC12
-cmp ren,321d
+cmp ren,323d
 ja etiC12
-mov color,11d; Se asigna color Negro
+mov color,13d ; Se asigna color morado bajito
 call ColSel
 jmp eti3
-etiC12:
-cmp col,14d
+etiC12: 
+
+
+cmp col,15d
 jb etiC13 ;JB=Jump if Below (Brinca si esta abajo)
 cmp col,35d
 ja etiC13 ;JA=Jmp if Above (Brinca si esta arriba)
-cmp ren,334d
+cmp ren,335d
 jb etiC13
-cmp ren,354d
+cmp ren,355d
 ja etiC13
-mov color,12d; Se asigna color Negro
+mov color,3d ; Se asigna color azul cielo 
 call ColSel
 jmp eti3
-etiC13:
+etiC13: 
+
+
 cmp col,42d
 jb etiC14 ;JB=Jump if Below (Brinca si esta abajo)
-cmp col,63d
+cmp col,62d
 ja etiC14 ;JA=Jmp if Above (Brinca si esta arriba)
-cmp ren,333d
+cmp ren,335d
 jb etiC14
-cmp ren,353d
+cmp ren,355d
 ja etiC14
-mov color,13d; Se asigna color Negro
+mov color,11d ; Se asigna color azul cian 
 call ColSel
 jmp eti3
+
 etiC14:
-cmp col,70d
+cmp col,71d
 jb etiC15 ;JB=Jump if Below (Brinca si esta abajo)
 cmp col,91d
 ja etiC15 ;JA=Jmp if Above (Brinca si esta arriba)
-cmp ren,333d
+cmp ren,335d
 jb etiC15
-cmp ren,353d
+cmp ren,355d
 ja etiC15
-mov color,14d; Se asigna color Negro
+mov color,8d ; Se asigna color gris oscuro
 call ColSel
 jmp eti3
+
 etiC15:
+
 cmp col,96d
 jb eti3 ;JB=Jump if Below (Brinca si esta abajo)
 cmp col,117d
 ja eti3 ;JA=Jmp if Above (Brinca si esta arriba)
-cmp ren,333d
+cmp ren,335d
 jb eti3
-cmp ren,389d
+cmp ren,355d
 ja eti3
-mov color,15d; Se asigna color Negro
+mov color,2d ; Se asigna color verde 
 call ColSel
 jmp eti3
+ 
 etiExt:
 mov ah,00h
 mov al,3d
@@ -422,7 +463,7 @@ int 33h
 mov ah,07h
 int 21h
 
-;***********************************************************************************************************************
+;***************
 err: ;Se llega hasta aqui solo si hay error en la lectura del archivo
  mov ah,09h
  lea dx,cad
@@ -431,7 +472,7 @@ err: ;Se llega hasta aqui solo si hay error en la lectura del archivo
  int 21h ;Espera a que se oprima tecla
  int 20h ;Fin del Programa (Cuando NO se carga la imagen)
 
-;***********************************************************************************************************************
+;***************
 ; Procedimiento que nos devuelve la posicion del cursor
 pos proc
  mov ah,02h
@@ -441,7 +482,7 @@ pos proc
  int 10h
  ret
 endp
-;***********************************************************************************************************************
+;***************
 ; Procedimiento necesario para poder dibujar el cuadrado
 cuadro proc
  mov cx,col1
@@ -512,3 +553,20 @@ eti10:
  jbe eti9
 ret
 endp
+ColFal proc
+ mov dx,335d 
+eti11:
+ mov cx,96d
+eti12: 
+ mov ah,0ch
+ mov al,2d
+ int 10h
+ inc cx
+ cmp cx,117d
+ jbe eti12 ;JBE=Jump if Below or Equal (Salta si esta abajo, o si es Igual)
+ inc dx
+ cmp dx,355d
+ jbe eti11
+ret
+endp
+Barril proc
